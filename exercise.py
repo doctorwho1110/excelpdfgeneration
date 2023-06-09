1e75e8cbe16ef2fa15b6948ac493d345f38a4dd7
doctorwho1110 <yeldanugur99@gmail.com>
2023-06-09 14:48:44 +0300

    Final commit of exercise.py and output.pdf

--- exercise.py
+++ exercise.py
@@ -14,9 +14,19 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     name = filename.title()
 
-    pdf.set_font(family="Times", style="B", size=24)
+    with open(filepath,"r") as file:
+        content=file.read()
+
+    pdf.set_font(family="Times", size=12)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=name, align="L",
              ln=1, border=0)
+
+    #Adding explanations on the .txt files
+    # for x in data:
+    #     pdf.cell(w=0, h=8, txt=x, align="L",
+    #              ln=1, border=0)
+
+    pdf.multi_cell(w=0,h=6,txt=content)
 
 pdf.output(f"PDFs/output.pdf")
